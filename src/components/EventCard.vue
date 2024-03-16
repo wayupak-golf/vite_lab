ceb923a218fa55ff640109e050efc36015ed7356
wayupak-golf <158698250+wayupak-golf@users.noreply.github.com>
2024-03-16 16:34:51 +0700

    Add files via upload

---
+++ src/components/EventCard.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="event-class">
+        <div class="event-card">
+            <span>@{{ event.time }} on {{ event.date }}</span>
+            <h4>{{ event.title }}</h4>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { EventItem } from '@/type';
+import { Proptype } from 'vue';
+const props = defineProps({
+    event: {
+        type: Object as PropType<EventItem>,
+        required: true
+    }
+
+})
+</script>
+
+<style scoped>
+.event-card {
+    padding: 20px;
+    width: 250px;
+    cursor: pointer;
+    border: 1px solid #39495c;
+    margin-bottom: 18px;
+}
+
+.event-card:hover {
+    transform: scale(1.01);
+    box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
+```
